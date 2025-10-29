421fe5aba5817fefc075bc3271000b7642617441
joyal777 <joyaltony78@gmail.com>
2025-10-29 22:55:32 +0530

    Update Show.vue

--- resources/js/pages/Tasks/Show.vue
+++ resources/js/pages/Tasks/Show.vue
@@ -178,7 +178,7 @@ const deleteTask = (task: any) => {
                         <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
                         </svg>
-                        Edit Task
+                        Edit
                     </Link>
                     <button
                         @click="deleteTask(task)"
@@ -198,7 +198,7 @@ const deleteTask = (task: any) => {
                         <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path>
                         </svg>
-                        Back to {{ currentProject.project_name }}
+                        {{ currentProject.project_name }}
                     </Link>
 
                 </div>
